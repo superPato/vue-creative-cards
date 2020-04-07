60b2d1fc0769c7b05b63f2c8d2fae18bab3dc6f2
Cesar Acual <augustoacual@gmail.com>
2020-04-07 12:27:22 -0600

    Create single file components

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-const component1 = {
-  template: '<p>Component 1</p>'
-};
-
-const component2 = {
-  template: '<p>Component 2</p>'
-};
-
 new Vue({
   el: '#app',
-  components: {
-    'component-1': component1,
-    'component-2': component2
-  }
-  // render: h => h(App)
+  render: h => h(App)
 })
