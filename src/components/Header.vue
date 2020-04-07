62699b5c0fcaaa4a54e957d347701b4cb1bc47f3
Cesar Acual <augustoacual@gmail.com>
2020-04-07 12:41:10 -0600

    Adding the card front component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
   <div>
     <h1 class="text-center">Creative Cards</h1>
     <ul class="nav justify-content-center">
-      <li class="nav-item">Front</li>
-      <li class="nav-item">Inside Left</li>
-      <li class="nav-item">Inside Right</li>
-      <li class="nav-item">Back</li>
+      <li class="nav-item"><a class="nav-link">Front</a></li>
+      <li class="nav-item"><a class="nav-link">Inside Left</a></li>
+      <li class="nav-item"><a class="nav-link">Inside Right</a></li>
+      <li class="nav-item"><a class="nav-link">Back</a></li>
     </ul>
   </div>
 </template>
@@ -17,5 +17,9 @@
 <style>
   h1 {
     margin-top: 10px;
+  }
+
+  a {
+    cursor: pointer;
   }
 </style>
