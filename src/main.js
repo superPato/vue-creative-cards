dae2efdad718e88ec0c9974d07eec48ba8905834
Cesar Acual <augustoacual@gmail.com>
2020-04-07 12:17:19 -0600

    Registering local components

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.component('text-input', {
-  template: '<textarea :placeholder="textValue" @click="changeText"></textarea>',
-  data: function () {
-    return {
-      textValue: 'Type here'
-    }
-  },
-  methods: {
-    changeText: function () {
-      this.textValue = 'Text changed';
-    }
-  }
-});
+const component1 = {
+  template: '<p>Component 1</p>'
+};
+
+const component2 = {
+  template: '<p>Component 2</p>'
+};
 
 new Vue({
   el: '#app',
+  components: {
+    'component-1': component1,
+    'component-2': component2
+  }
   // render: h => h(App)
 })
