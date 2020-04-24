bdd3f78be6b96a9951e8830784806eccc096ed3c
Cesar Acual <augustoacual@gmail.com>
2020-04-23 18:20:43 -0600

    Setup Firebase

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import FirebaseConfig from './firebaseConfig.js';
 import Header from './components/Header.vue';
 import CardFront from './components/card/CardFront.vue';
 import CardInsideLeft from './components/card/CardInsideLeft.vue';
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import Firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCKD5tlgY-Aac0WOO9DDF6owOZsGBmeMbU",
+    authDomain: "creative-cards-app-4f8f9.firebaseapp.com",
+    databaseURL: "https://creative-cards-app-4f8f9.firebaseio.com",
+    projectId: "creative-cards-app-4f8f9",
+    storageBucket: "creative-cards-app-4f8f9.appspot.com",
+    messagingSenderId: "891885690221",
+    appId: "1:891885690221:web:8d50ff29030f0a414f0e64"
+  };
+  // Initialize Firebase
+  Firebase.initializeApp(firebaseConfig);
