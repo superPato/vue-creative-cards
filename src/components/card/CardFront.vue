8726ac2b700cff3a46ff29ab28f4b0ec33898397
Cesar Acual <augustoacual@gmail.com>
2020-04-25 12:32:50 -0600

    Upload progress bar and emit file data

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -2,11 +2,12 @@
   <div class="row">
 		<div class="col-sm-6 card edit-area">
 			<cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
-			<cc-image-upload></cc-image-upload><hr>
+			<cc-image-upload @displayImageChanged="imageName = $event"></cc-image-upload><hr>
 			<cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
 			<cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
 		</div>
 		<div class="col-sm-6 card edit-display">
+			{{ imageName }}
 			<cc-text-output :displayText="textBoxValue1" :containerHeight="130"></cc-text-output>
 			<cc-text-output :displayText="textBoxValue2" :containerHeight="130"></cc-text-output>
 			<cc-text-output :displayText="textBoxValue3" :containerHeight="130"></cc-text-output>
@@ -25,6 +26,7 @@ export default {
 			textBoxValue1: '',
 			textBoxValue2: '',
 			textBoxValue3: '',
+			imageName: '',
 		}
 	},
 	components: {
--- src/components/card/ImageUpload.vue
+++ src/components/card/ImageUpload.vue
@@ -5,6 +5,7 @@
             <div class="form-group">
                 <input type="file" class="form-control-file" id="fileUpload" @change="uploadFile">
             </div>
+            <progress value="0" max="100" id="progressBar"></progress>
             <br>
             <img id="image">
             <button type="button" id="setImageButton">Set Image</button>
@@ -25,14 +26,23 @@ export default {
         uploadFile: function (event) {
             this.file = event.target.files[0]
             var storageRef = Firebase.storage().ref('user_uploads/' + this.file.name)
-            storageRef.put(this.file)
+            var upload = storageRef.put(this.file)
 
+            // create thumbnail
             var reader = new FileReader();
             reader.readAsDataURL(this.file)
 
             reader.onload = function (e) {
                 document.getElementById('image').src = e.target.result
             }
+
+            // progress bar
+            upload.on('state_changed', function (snapshot) {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                document.getElementById('progressBar').value = progress;
+            });
+
+            this.$emit('displayImageChanged', this.file.name)
         }
     }
 }
