11c8b1d20e7fc02f02ec77383eb94d1a19a7ba39
Cesar Acual <augustoacual@gmail.com>
2020-05-05 14:52:20 -0600

    Text options menu font sizes

--- src/components/card/TextOutput.vue
+++ src/components/card/TextOutput.vue
@@ -1,6 +1,36 @@
 <template>
-    <div>
-        <p :style="styleObject">{{ displayText }}</p>
+    <div @mouseover="showOptions = true"
+         @mouseleave="showOptions = false">
+        
+        <form class="form-inline" v-show="showOptions">
+            <label for="selectBox">Font size: </label>
+            <select id="selectBox" class="custom-select" v-model="setFontSize">
+                <option value="42">42px</option>
+                <option value="48">48px</option>
+                <option value="56">56px</option>
+                <option value="64">64px</option>
+            </select>
+
+            <div class="form-check form-check-inline">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" value="left" v-model="setTextAlign"> Left
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" value="center" v-model="setTextAlign"> Center
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" value="right" v-model="setTextAlign"> Right
+                </label>
+            </div>
+        </form>
+
+        <p :style="styleObject">
+            {{ displayText }}
+        </p>
     </div>
 </template>
 
@@ -13,9 +43,18 @@ export default {
             default: 200
         }
     },
+    data: function() {
+        return {
+            showOptions: false,
+            setFontSize: '',
+            setTextAlign: '',
+        }
+    },
     computed: {
         styleObject: function () {
             return {
+                textAlign: this.setTextAlign,
+                fontSize: this.setFontSize + 'px',
                 height: this.containerHeight + 'px'
             }
         }
